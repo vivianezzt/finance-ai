b8ff9259d95c29d9e86db7dba2622ce44a0bcfea
Viviane <vivianeaguiarc@outlook.com>
2024-12-17 15:05:39 -0300

    feat: add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import { Button } from "../_components/ui/button";
+import { LogInIcon } from "lucide-react";
+
+const LoginPage = () => {
+  return (
+    <div className="grid h-full grid-cols-2">
+      {/* Left side */}
+      <div className="flex h-full max-w-[550px] flex-col justify-center p-8">
+        <Image
+          src="/logo.svg"
+          width={173}
+          height={39}
+          alt="Finance Ai"
+          className="mb-8"
+        />
+        <h1 className="mb-3 text-4xl font-bold">Bem vindo</h1>
+        <p className="mb-8 text-muted-foreground">
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
+        </p>
+        <Button variant="outline">
+          <LogInIcon />
+          Fazer login ou criar conta
+        </Button>
+      </div>
+      {/* Right side */}
+      <div className="relative h-full w-full">
+        <Image
+          src="/login.png"
+          alt="Faça login"
+          fill
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
